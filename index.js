7a08b1546a18cb7336b4cabd90ed5646c4be419b
Cristian Rodriguez <cristian6569@gmail.com>
2019-10-27 16:52:41 -0500

    Project, About made and Contact screen started

--- index.js
+++ index.js
@@ -8,11 +8,137 @@ let app = express();
 
 let database = [
     {
-        id: "code_design",
-        name: "Code Design",
-        category: "Web Redesign"
+        id: "ice_cream",
+        field: "FrontEnd - UI Design",
+        name: "Ice Cream Nation",
+        category: "Web Redesign",
+        introtextnormal: "In the redesign of the Ice Cream Nation website had the challenge to ",
+        introtextbold: "create an experience that interest people and atract more customers.",
+        bannermock: "../assets/projects/iceCream/bannerMockUp.png",
+        introimage: "../assets/projects/iceCream/intro.png",
     }
 ]
+
+const competencies= [
+    {
+        photo: "../assets/about/competencies/develop.png",
+        name: "Web Development"
+    },
+    {
+        photo: "../assets/about/competencies/rapid.png",
+        name: "Rapid Prototyping"
+    },
+    {
+        photo: "../assets/about/competencies/research.png",
+        name: "Design Research"
+    },
+    {
+        photo: "../assets/about/competencies/data.png",
+        name: "Data Viz"
+    },
+    {
+        photo: "../assets/about/competencies/interaction.png",
+        name: "Interaction Design"
+    },
+]
+
+let tools = [
+    {
+        icon: "../assets/about/tools/figma.png",
+        name: "Figma",
+        category: "des"
+    },
+    {
+        icon: "../assets/about/tools/invision.png",
+        name: "InVision",
+        category: "des"
+    },
+    {
+        icon: "../assets/about/tools/illustrator.png",
+        name: "Adobe Illustrator",
+        category: "des"
+    },
+    {
+        icon: "../assets/about/tools/photoshop.png",
+        name: "Adobe Photoshop",
+        category: "des"
+    },
+    {
+        icon: "../assets/about/tools/muse.png",
+        name: "Adobe Muse",
+        category: "des"
+    },
+    {
+        icon: "../assets/about/tools/lightroom.png",
+        name: "Adobe Lightroom",
+        category: "des"
+    },
+    {
+        icon: "../assets/about/tools/premiere.png",
+        name: "Adobe Premiere",
+        category: "des"
+    },
+    {
+        icon: "../assets/about/tools/html.png",
+        name: "HTML",
+        category: "dev"
+    },
+    {
+        icon: "../assets/about/tools/javascript.png",
+        name: "Javascript",
+        category: "dev"
+    },
+    {
+        icon: "../assets/about/tools/css.png",
+        name: "Css",
+        category: "dev"
+    },
+    {
+        icon: "../assets/about/tools/sass.png",
+        name: "Sass",
+        category: "dev"
+    },
+    {
+        icon: "../assets/about/tools/react.png",
+        name: "React",
+        category: "dev"
+    },
+    {
+        icon: "../assets/about/tools/angular.png",
+        name: "Angular",
+        category: "dev"
+    },
+    {
+        icon: "../assets/about/tools/ts.png",
+        name: "Typescript",
+        category: "dev"
+    },
+    {
+        icon: "../assets/about/tools/processing.png",
+        name: "P5.js",
+        category: "dev"
+    },
+    {
+        icon: "../assets/about/tools/mobx.png",
+        name: "Mobx",
+        category: "dev"
+    },
+    {
+        icon: "../assets/about/tools/firebase.png",
+        name: "Firebase",
+        category: "dev"
+    },
+    {
+        icon: "../assets/about/tools/visual.png",
+        name: "Visual Studio Code",
+        category: "dev"
+    },
+    {
+        icon: "../assets/about/tools/git.png",
+        name: "Git",
+        category: "dev"
+    },
+];
 
 app.engine('hbs', engines.handlebars);
 app.set('views', './views');
@@ -21,7 +147,20 @@ app.set('view engine', 'hbs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {
+        projects: database
+    });
+});
+
+app.get('/AboutMe', (req, res) => {
+    res.render('about', {
+        competencies: competencies,
+        tools: tools
+    });
+});
+
+app.get('/Contact', (req, res) => {
+    res.render('contact');
 });
 
 
@@ -33,7 +172,11 @@ app.get('/projects/:name', (req, res) => {
     
     res.render('project', {
         name: project.name,
-        category: project.category
+        category: project.category,
+        introtextnormal: project.introtextnormal,
+        introtextbold: project.introtextbold,
+        bannermock: project.bannermock,
+        introimage: project.introimage,
     });
 });
 
--- public/js/uifeatures.js
+++ public/js/uifeatures.js
@@ -1,4 +1,10 @@
 var menu_elements = Array.from(document.querySelector("nav ul").children);
 
 
+var item = document.querySelector('body');
 
+window.addEventListener('wheel', function(e) {
+    console.log(item.scrollTop)
+  if (e.deltaY > 0) item.scrollLeft += 100;
+  else item.scrollLeft -= 100;
+});
